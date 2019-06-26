d29fdd4fee5dd2fc2b2473d49218c5d4ef3f0ee5
Ammar <ammar-rasheed@hotmail.com>
2019-06-26 02:16:34 +0200

    Initial commit

---
+++ Tfrd/app/src/main/java/com/example/tfrd/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.tfrd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.activity_main.view.textView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun clecamare(view : View){
+    }
+}
